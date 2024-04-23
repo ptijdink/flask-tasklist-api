c085d1b7d22423080daf0bd436d7bd7dc967d979
Peter Tijdink <petertijdink@Peters-MacBook-Pro.local>
2024-04-23 13:10:15 +0200

    Implement functionality to delete tasks (WIP)
    Added the ability to delete tasks based on their IDs. However, there are issues with the deletion process, such as deleting additional tasks unintentionally. Further debugging and refinement are required.

--- app/routes.py
+++ app/routes.py
@@ -28,3 +28,23 @@ def register_routes(app, db):
 
             tasks = Task.query.all()
             return render_template('index.html', tasks=tasks)
+
+
+    @app.route('/delete/<pid>', methods=['GET'])
+    def delete(pid):
+        """Delete a task based on the given ID.
+
+        Args:
+            pid (str): The ID of the task to be deleted.
+
+        Returns:
+            str: An HTML template with the updated list of tasks after deleting the task.
+        """
+        Task.query.filter(Task.pid == pid).delete()
+
+        db.session.commit()
+
+        tasks = Task.query.all()
+        return render_template('index.html', tasks=tasks)
+
+    
